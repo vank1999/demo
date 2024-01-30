a3a20019f717cd6972a6061148d5e9605104c0bb
vank1999 <156069219+vank1999@users.noreply.github.com>
2024-01-30 20:00:47 +0530

    Update setup_student_app.sh

--- setup_student_app.sh
+++ setup_student_app.sh
@@ -37,3 +37,48 @@ tar -xzvf apache-maven-3.9.6-bin.tar.gz
 mv apache-maven-3.9.6 maven
 rm -rf apache-maven-3.9.6-bin.tar.gz
 sudo su - devops -c "ln -s /opt/maven/bin/mvn /usr/local/bin/mvn"
+
+wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.85/bin/apache-tomcat-9.0.85.tar.gz
+tar -xzvf apache-tomcat-9.0.85.tar.gz
+rm -rf apache-tomcat-9.0.85.tar.gz
+mv apache-tomcat-9.0.85 appserver
+
+echo '[Unit]
+        Description=Tomcat Server
+        After=syslog.target network.target
+
+        [Service]
+        Type=forking
+        User=devops
+        Group=devops
+
+        Environment=JAVA_HOME=/usr/lib/jvm/java-11-openjdk-11.0.13.0.8-4.el8_5.x86_64
+        Environment='JAVA_OPTS=-Djava.awt.headless=true'
+        Environment=CATALINA_HOME=/opt/appserver/
+        Environment=CATALINA_BASE=/opt/appserver/
+        Environment=CATALINA_PID=/opt/appserver/temp/tomcat.pid
+        Environment='CATALINA_OPTS=-Xms512M -Xmx512M'
+        ExecStart=/opt/appserver/bin/catalina.sh start
+        ExecStop=/opt/appserver/bin/catalina.sh stop
+
+        [Install]
+        WantedBy=multi-user.target' > /etc/systemd/system/tomcat.service
+
+systemctl daemon-reload
+systemctl start tomcat
+systemctl enable tomcat
+
+# Setup Nginx
+
+sudo yum install nginx.x86_64 -y
+systemctl enable nginx
+systemctl start nginx
+
+# Setup Maria DB
+
+sudo yum install mariadb-server.x86_64 -y
+systemctl enable mariadb
+systemctl start mariadb
+# systemctl status mariadb
+
+git clone https://gitlab.com/rns-app/student-app.git
